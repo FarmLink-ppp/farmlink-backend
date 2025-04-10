770b27e18cae23cb01dcc4e86cf0cc574319ba53
abderbj <abderrahman.bejaoui@insat.ucar.tn>
2025-04-10 16:10:10 +0100

    crud on posts

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,51 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete , UseGuards, Req} from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import {
+  ApiBearerAuth,
+  ApiCookieAuth,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RequestWithUser } from 'src/common/types/auth.types';
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+  @ApiCookieAuth('access-token')
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  create(@Body() createPostDto: CreatePostDto, @Req() req: RequestWithUser) {
+    const userId = req.user.id; 
+      return this.postsService.create(userId, createPostDto);
+
+  }
+  @ApiCookieAuth('access-token')
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  findAll() {
+    return this.postsService.findAll();
+  }
+  @ApiCookieAuth('access-token')
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.postsService.findOne(+id);
+  }
+  @ApiCookieAuth('access-token')
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+    return this.postsService.update(+id, updatePostDto);
+  }
+  @ApiCookieAuth('access-token')
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.postsService.remove(+id);
+  }
+}
