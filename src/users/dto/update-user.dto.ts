7f41de3aa4ae05bf3ba2fbdd1a63c5dc1ae43be2
Yasmine Ouerghi <yasmine.ouerghi15@gmail.com>
2025-04-10 15:47:09 +0100

    feat: add update profile feature

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,19 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiPropertyOptional({
+    description: 'The user account type',
+    example: 'PUBLIC',
+    required: false,
+  })
+  @IsOptional()
+  accountType?: AccountType;
+  
+}
+
+enum AccountType {
+  PUBLIC = 'PUBLIC',
+  PRIVATE = 'PRIVATE',
+}
