d3894f3c138259bf35fbb0a20951f884b9b73a9d
abderbj <abderrahman.bejaoui@insat.ucar.tn>
2025-04-11 17:14:26 +0100

    adding like comment share unlike unshare and get feed

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -8,6 +8,9 @@ import {
 } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RequestWithUser } from 'src/common/types/auth.types';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { LikePostDto } from './dto/like-post.dto';
+import { SharePostDto } from './dto/share-post.dto';
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
@@ -27,12 +30,73 @@ export class PostsController {
   findAll() {
     return this.postsService.findAll();
   }
+
+  @ApiCookieAuth('access-token')
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(JwtAuthGuard)
+  @Get('feed')
+  getFeed(@Req() req: RequestWithUser) {
+    return this.postsService.getFeed(req.user.id);
+  }
+  @ApiCookieAuth('access-token')
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(JwtAuthGuard)
+  @Post('comment')
+  createComment(@Body() dto: CreateCommentDto, @Req() req: RequestWithUser) {
+    return this.postsService.createComment(req.user.id, dto);
+  }
+
+  @ApiCookieAuth('access-token')
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(JwtAuthGuard)
+  @Post('like')
+  likePost(@Body() dto: LikePostDto, @Req() req: RequestWithUser) {
+    return this.postsService.likePost(req.user.id, dto);
+  }
+
+  @ApiCookieAuth('access-token')
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(JwtAuthGuard)
+  @Delete('like/:postId')
+  unlikePost(@Param('postId') postId: string, @Req() req: RequestWithUser) {
+    return this.postsService.unlikePost(req.user.id, Number(postId));
+  }
+
+  @ApiCookieAuth('access-token')
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(JwtAuthGuard)
+  @Post('share')
+  sharePost(@Body() dto: SharePostDto, @Req() req: RequestWithUser) {
+    return this.postsService.sharePost(req.user.id, dto);
+  }
+  @ApiCookieAuth('access-token')
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(JwtAuthGuard)
+  @Delete('share/:postId')
+  unsharePost(@Param('postId') postId: string, @Req() req: RequestWithUser) {
+    return this.postsService.unsharePost(req.user.id, Number(postId));
+  }
+
+  @Get(':postId/comments')
+  getPostComments(@Param('postId') postId: string) {
+    return this.postsService.getPostComments(Number(postId));
+  }
+
+  @Get(':postId/likes')
+  getPostLikes(@Param('postId') postId: string) {
+    return this.postsService.getPostLikes(Number(postId));
+  }
+
+  @Get(':postId/shares')
+  getPostShares(@Param('postId') postId: string) {
+    return this.postsService.getPostShares(Number(postId));
+  }
   @ApiCookieAuth('access-token')
   @ApiBearerAuth('JWT-auth')
   @UseGuards(JwtAuthGuard)
   @Get(':id')
-  findOne(@Param('id') id: string, @Req() req: RequestWithUser) {
-    return this.postsService.findOne(req.user.id,+id);
+  findOne(@Param('id') id: number, @Req() req: RequestWithUser) {
+    return this.postsService.findOne(req.user.id,id);
   }
   @ApiCookieAuth('access-token')
   @ApiBearerAuth('JWT-auth')
