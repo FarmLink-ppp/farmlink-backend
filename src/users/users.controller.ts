4124961fc090e0eb784247dafdc2babc64f25f36
racem kchaou <racem.kchaou@insat.ucar.tn>
2025-04-17 21:42:23 +0100

    fixing the image profile

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,15 @@
-import { Body, Controller, Get, Param, Put, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get,UseInterceptors, UploadedFile, Param, Put, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RequestWithUser } from 'src/common/types/auth.types';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { Express } from 'express';
+import {ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -22,5 +28,38 @@ export class UsersController {
   @Put('/profile')
   async updateProfile(@Req() req: RequestWithUser,@Body() updateUserDto: UpdateUserDto) {
     return await this.usersService.update(req.user.id,updateUserDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put('/upload-profile-image')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads/profile-images',
+        filename: (req, file, cb) => {
+          const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+        },
+      }),
+      fileFilter: (req, file, cb) => {
+        if (!file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
+          cb(new Error('Only image files are allowed!'), false);
+        } else {
+          cb(null, true);
+        }
+      },
+    }),
+  )
+  @ApiOperation({ summary: 'Update user profile image' })
+  @ApiResponse({ status: 200, description: 'Profile image updated successfully' })
+  async uploadProfileImage(
+    @UploadedFile() file: Express.Multer.File,
+    @Req() req: RequestWithUser,
+  ) {
+    const imageUrl = `http://localhost:3000/uploads/profile-images/${file.filename}`;
+    return this.usersService.update(req.user.id, {
+      profileImage: imageUrl,
+    });
   }
 }
