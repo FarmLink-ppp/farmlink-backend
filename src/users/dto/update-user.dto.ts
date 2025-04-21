e828d990dc51111b570b85742d7925ed43a6dea7
tarek-gritli <tarek.gritli@insat.ucar.tn>
2025-04-21 19:51:29 +0100

    fix: fix circular dependency error

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,13 +1,15 @@
 import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
-import { IsOptional } from 'class-validator';
+import { IsEnum, IsOptional } from 'class-validator';
 import { AccountType } from '@prisma/client';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @ApiPropertyOptional({
     description: 'The user account type',
     example: 'PUBLIC',
+    enum: AccountType,
   })
   @IsOptional()
+  @IsEnum(AccountType)
   accountType?: AccountType;
 }
