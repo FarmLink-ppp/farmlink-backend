9038d4b27ec05c3ae4bc7c7d6ef65f32d63b89de
tarek-gritli <tarek.gritli@insat.ucar.tn>
2025-06-08 17:12:03 +0100

    fix: set optional property to required

--- src/users/dto/update-password.dto.ts
+++ src/users/dto/update-password.dto.ts
@@ -1,8 +1,8 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class UpdatePasswordDto {
-  @ApiPropertyOptional({
+  @ApiProperty({
     description: 'The current password of the user',
     example: 'oldPassword123',
     type: String,
